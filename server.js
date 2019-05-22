2954aa2fd4b057a50903fd851bb68705bb5c5c83
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-05-21 17:01:59 -0700

    middleware complete, not tested

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 8080;
+
+const routes = require('./lib/routes.js');
+const logger = require('./lib/middleware/logger.js');
+const notFound = require('./lib/middleware/notFound.js');
+const errorHandler = require('./lib/middleware/errorHandler.js');
+const addReqTime = require('./lib/middleware/addReqTime.js')
+const squareNum = require('./middleware/squareNum.js');
+
+app.use(addReqTime);
+app.use(logger);
 
 app.get('/a', (req,res) => {
   res.status(200).send('Route A');
@@ -14,13 +22,15 @@ app.get('/b', (req,res) => {
   res.status(200).send('Route B');
 });
 
-app.get('/c', (req,res) => {
-  res.status(200).send('Route C');
+app.get('/b/:number', squareNum, (req,res) => {
+  res.status(200).send(req.number);
 });
 
-app.get('/d', (req,res) => {
-  res.status(200).send('Route D');
-});
+app.use('*', notFound);
+
+app.use('/', routes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
